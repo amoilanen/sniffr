be5339438b6fa8d691fcc369123ce1b106334001
anton <anton.al.ivanov@gmail.com>
2014-07-16 00:30:10 +0300

    Added new failing tests for Internet Explorer

--- spec/sniffr.spec.js
+++ spec/sniffr.spec.js
@@ -1,6 +1,58 @@
 describe("sniffr", function() {
 
-  it("it should work", function() {
-    expect(true).toBe(true);
+  var sniffer;
+
+  beforeEach(function() {
+    sniffer = new Sniffr();
   });
+
+  function property(name, versionString) {
+    return {
+      name: name,
+      version: versionString.split(".").map(function(versionPart) {
+        return parseInt(versionPart);
+      }),
+      versionString: versionString
+    };
+  }
+
+  function os(name, versionString) {
+    return property(name, versionString);
+  }
+
+  function browser(name, versionString) {
+    return property(name, versionString);
+  }
+
+  function shouldDetect(os, browser, agentString) {
+    var specName = "os " + os.name + " " + os.versionString + 
+      ", browser" + browser.name + " " + browser.versionString;
+
+    describe(specName, function() {
+      it("it should recognize", function() {
+        sniffer.sniff();
+        expect(sniffer.getOS()).toEqual(os);
+        expect(sniffer.getBrowser()).toEqual(browser);
+      });
+    });
+  };
+
+  describe("InternetExplorer", function() {
+    shouldDetect(os("Windows", "6.3"), browser("InternetExplorer", "11.0"),
+      "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko");
+    shouldDetect(os("Windows", "6.1"), browser("InternetExplorer", "10.0"),
+      "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)");
+    shouldDetect(os("Windows", "6.1"), browser("InternetExplorer", "9.0"),
+      "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0) chromeframe/10.0.648.205");
+    shouldDetect(os("Windows", "5.1"), browser("InternetExplorer", "8.0"),
+      "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)");
+  });
+
+  //TODO: Firefox
+  //TODO: Chrome
+  //TODO: Safari
+
+  //TODO: No user agent string available
+  //TODO: Unknown agent string
+  //TODO: No analysis has been run before getting the properties
 });
--- src/sniffr.js
+++ src/sniffr.js
@@ -1,1 +1,27 @@
-console.log("Sniffr will be implemented here");
+(function(host) {
+
+  var UNKNOWN_PROPERTY = {
+    name: "Unknown",
+    version: [],
+    versionString: "Unknown"
+  };
+
+  function Sniffr() {
+    this.browser = UNKNOWN_PROPERTY;
+    this.os = UNKNOWN_PROPERTY;
+  }
+
+  Sniffr.prototype.sniff = function() {
+    //TODO: Implement
+  };
+
+  Sniffr.prototype.getBrowser = function() {
+    return this.browser;
+  };
+
+  Sniffr.prototype.getOS = function() {
+    return this.os;
+  };
+
+  host.Sniffr = Sniffr;
+})(this);
