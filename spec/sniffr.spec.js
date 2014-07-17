b2d29fa28de30549affbe65c80f7319692fef97f
anton <anton.al.ivanov@gmail.com>
2014-07-17 23:55:41 +0300

    First tests are passing

--- spec/sniffr.spec.js
+++ spec/sniffr.spec.js
@@ -25,12 +25,12 @@ describe("sniffr", function() {
   }
 
   function shouldDetect(os, browser, agentString) {
-    var specName = "os " + os.name + " " + os.versionString + 
+    var specName = "os " + os.name + " " + os.versionString +
       ", browser" + browser.name + " " + browser.versionString;
 
     describe(specName, function() {
       it("it should recognize", function() {
-        sniffer.sniff();
+        sniffer.sniff(agentString);
         expect(sniffer.getOS()).toEqual(os);
         expect(sniffer.getBrowser()).toEqual(browser);
       });
@@ -38,17 +38,18 @@ describe("sniffr", function() {
   };
 
   describe("InternetExplorer", function() {
-    shouldDetect(os("Windows", "6.3"), browser("InternetExplorer", "11.0"),
+    shouldDetect(os("windows", "6.3"), browser("ie", "11.0"),
       "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko");
-    shouldDetect(os("Windows", "6.1"), browser("InternetExplorer", "10.0"),
+    shouldDetect(os("windows", "6.1"), browser("ie", "10.0"),
       "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)");
-    shouldDetect(os("Windows", "6.1"), browser("InternetExplorer", "9.0"),
+    shouldDetect(os("windows", "6.1"), browser("ie", "9.0"),
       "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0) chromeframe/10.0.648.205");
-    shouldDetect(os("Windows", "5.1"), browser("InternetExplorer", "8.0"),
+    shouldDetect(os("windows", "5.1"), browser("ie", "8.0"),
       "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)");
   });
 
   //TODO: Firefox
+  //"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0"
   //TODO: Chrome
   //TODO: Safari
 
--- src/sniffr.js
+++ src/sniffr.js
@@ -6,13 +6,60 @@
     versionString: "Unknown"
   };
 
+  var matchers = {
+    browser: [
+      [/msie ([\.\_\d]+)/, "ie"],
+      [/trident\/.*?rv:([\.\_\d]+)/, "ie"]
+    ],
+    os: [
+      [/windows nt ([\.\_\d]+)/, "windows"]
+    ]
+  };
+
   function Sniffr() {
     this.browser = UNKNOWN_PROPERTY;
     this.os = UNKNOWN_PROPERTY;
   }
 
-  Sniffr.prototype.sniff = function() {
-    //TODO: Implement
+  function determineProperty(self, propertyName, userAgent) {
+    var propertyMatchers = matchers[propertyName];
+
+    for (var i = 0; i < propertyMatchers.length; i++) {
+      var propertyRegex = propertyMatchers[i][0];
+      var propertyValue = propertyMatchers[i][1];
+
+      var match = userAgent.match(propertyRegex);
+
+      if (match) {
+        self[propertyName] = {
+          name: propertyValue,
+          versionString: match[1],
+          version: parseVersion(match[1])
+        };
+      }
+    }
+  };
+
+  function parseVersion(versionString) {
+    var versionParts = versionString.split(/[\._]/);
+    var version = [];
+
+    for (var i = 0; i < versionParts.length; i++) {
+      version.push(parseInt(versionParts[i]));
+    }
+    return version;
+  }
+
+  Sniffr.prototype.sniff = function(userAgentString) {
+    var userAgentString = userAgentString || navigator.userAgent;
+
+    if (!userAgentString) {
+      return;
+    }
+    userAgentString = userAgentString.toLowerCase();
+
+    determineProperty(this, "os", userAgentString);
+    determineProperty(this, "browser", userAgentString);
   };
 
   Sniffr.prototype.getBrowser = function() {
