04b1488599f04054e0a159cad21b41f88e2ebf8c
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-12-12 22:15:14 +0200

    Concatenate and minify files automatically, remove obsolete Grunt build file

--- dist/sniffr.js
+++ dist/sniffr.js
@@ -33,58 +33,58 @@ if (!Object.keys) {
 
   var properties = {
     browser: [
-      [/msie ([\.\_\d]+)/, "ie"],
-      [/trident\/.*?rv:([\.\_\d]+)/, "ie"],
-      [/firefox\/([\.\_\d]+)/, "firefox"],
-      [/chrome\/([\.\_\d]+)/, "chrome"],
-      [/version\/([\.\_\d]+).*?safari/, "safari"],
-      [/mobile safari ([\.\_\d]+)/, "safari"],
-      [/android.*?version\/([\.\_\d]+).*?safari/, "com.android.browser"],
-      [/crios\/([\.\_\d]+).*?safari/, "chrome"],
-      [/opera/, "opera"],
-      [/opera\/([\.\_\d]+)/, "opera"],
-      [/opera ([\.\_\d]+)/, "opera"],
-      [/opera mini.*?version\/([\.\_\d]+)/, "opera.mini"],
-      [/opios\/([a-z\.\_\d]+)/, "opera"],
-      [/blackberry/, "blackberry"],
-      [/blackberry.*?version\/([\.\_\d]+)/, "blackberry"],
-      [/bb\d+.*?version\/([\.\_\d]+)/, "blackberry"],
-      [/rim.*?version\/([\.\_\d]+)/, "blackberry"],
-      [/iceweasel\/([\.\_\d]+)/, "iceweasel"],
-      [/edge\/([\.\d]+)/, "edge"]
+      [/msie ([\.\_\d]+)/, 'ie'],
+      [/trident\/.*?rv:([\.\_\d]+)/, 'ie'],
+      [/firefox\/([\.\_\d]+)/, 'firefox'],
+      [/chrome\/([\.\_\d]+)/, 'chrome'],
+      [/version\/([\.\_\d]+).*?safari/, 'safari'],
+      [/mobile safari ([\.\_\d]+)/, 'safari'],
+      [/android.*?version\/([\.\_\d]+).*?safari/, 'com.android.browser'],
+      [/crios\/([\.\_\d]+).*?safari/, 'chrome'],
+      [/opera/, 'opera'],
+      [/opera\/([\.\_\d]+)/, 'opera'],
+      [/opera ([\.\_\d]+)/, 'opera'],
+      [/opera mini.*?version\/([\.\_\d]+)/, 'opera.mini'],
+      [/opios\/([a-z\.\_\d]+)/, 'opera'],
+      [/blackberry/, 'blackberry'],
+      [/blackberry.*?version\/([\.\_\d]+)/, 'blackberry'],
+      [/bb\d+.*?version\/([\.\_\d]+)/, 'blackberry'],
+      [/rim.*?version\/([\.\_\d]+)/, 'blackberry'],
+      [/iceweasel\/([\.\_\d]+)/, 'iceweasel'],
+      [/edge\/([\.\d]+)/, 'edge']
     ],
     os: [
-      [/linux ()([a-z\.\_\d]+)/, "linux"],
-      [/mac os x/, "macos"],
-      [/mac os x.*?([\.\_\d]+)/, "macos"],
-      [/os ([\.\_\d]+) like mac os/, "ios"],
-      [/openbsd ()([a-z\.\_\d]+)/, "openbsd"],
-      [/android/, "android"],
-      [/android ([a-z\.\_\d]+);/, "android"],
-      [/mozilla\/[a-z\.\_\d]+ \((?:mobile)|(?:tablet)/, "firefoxos"],
-      [/windows\s*(?:nt)?\s*([\.\_\d]+)/, "windows"],
-      [/windows phone.*?([\.\_\d]+)/, "windows.phone"],
-      [/windows mobile/, "windows.mobile"],
-      [/blackberry/, "blackberryos"],
-      [/bb\d+/, "blackberryos"],
-      [/rim.*?os\s*([\.\_\d]+)/, "blackberryos"]
+      [/linux ()([a-z\.\_\d]+)/, 'linux'],
+      [/mac os x/, 'macos'],
+      [/mac os x.*?([\.\_\d]+)/, 'macos'],
+      [/os ([\.\_\d]+) like mac os/, 'ios'],
+      [/openbsd ()([a-z\.\_\d]+)/, 'openbsd'],
+      [/android/, 'android'],
+      [/android ([a-z\.\_\d]+);/, 'android'],
+      [/mozilla\/[a-z\.\_\d]+ \((?:mobile)|(?:tablet)/, 'firefoxos'],
+      [/windows\s*(?:nt)?\s*([\.\_\d]+)/, 'windows'],
+      [/windows phone.*?([\.\_\d]+)/, 'windows.phone'],
+      [/windows mobile/, 'windows.mobile'],
+      [/blackberry/, 'blackberryos'],
+      [/bb\d+/, 'blackberryos'],
+      [/rim.*?os\s*([\.\_\d]+)/, 'blackberryos']
     ],
     device: [
-      [/ipad/, "ipad"],
-      [/iphone/, "iphone"],
-      [/lumia/, "lumia"],
-      [/htc/, "htc"],
-      [/nexus/, "nexus"],
-      [/galaxy nexus/, "galaxy.nexus"],
-      [/nokia/, "nokia"],
-      [/ gt\-/, "galaxy"],
-      [/ sm\-/, "galaxy"],
-      [/xbox/, "xbox"],
-      [/(?:bb\d+)|(?:blackberry)|(?: rim )/, "blackberry"]
+      [/ipad/, 'ipad'],
+      [/iphone/, 'iphone'],
+      [/lumia/, 'lumia'],
+      [/htc/, 'htc'],
+      [/nexus/, 'nexus'],
+      [/galaxy nexus/, 'galaxy.nexus'],
+      [/nokia/, 'nokia'],
+      [/ gt\-/, 'galaxy'],
+      [/ sm\-/, 'galaxy'],
+      [/xbox/, 'xbox'],
+      [/(?:bb\d+)|(?:blackberry)|(?: rim )/, 'blackberry']
     ]
   };
 
-  var UNKNOWN = "Unknown";
+  var UNKNOWN = 'Unknown';
 
   var propertyNames = Object.keys(properties);
 
@@ -114,7 +114,7 @@ if (!Object.keys) {
           self[propertyName].versionString = match[2];
           self[propertyName].version = [];
         } else if (match[1]) {
-          self[propertyName].versionString = match[1].replace(/_/g, ".");
+          self[propertyName].versionString = match[1].replace(/_/g, '.');
           self[propertyName].version = parseVersion(match[1]);
         } else {
           self[propertyName].versionString = UNKNOWN;
@@ -132,7 +132,9 @@ if (!Object.keys) {
 
   Sniffr.prototype.sniff = function(userAgentString) {
     var self = this;
-    var userAgent = (userAgentString || navigator.userAgent || "").toLowerCase();
+    var isBrowser = typeof window !== 'undefined';
+    var fallbackUserAgent = isBrowser ? navigator.userAgent : '';
+    var userAgent = (userAgentString || fallbackUserAgent).toLowerCase();
 
     propertyNames.forEach(function(propertyName) {
       determineProperty(self, propertyName, userAgent);
